4bca94b654e2e12c8919bf1f465fb780792a4c2a
yang li <1488476@qq.com>
2020-03-09 14:49:32 +0800

    移动了一下已经用过的sql文件

---
+++ ScientificResearch/文档/数据库升级文档/已经同步的/从test库第一次导入特定的文件夹下的试题.sql
@@ -0,0 +1,62 @@
+--要导入的文件夹
+SET IDENTITY_INSERT 继教文件夹 ON;
+
+WITH cte AS (
+SELECT * FROM ScientificResearch_Test.dbo.继教文件夹  WHERE 名称='共享试题'
+UNION ALL
+SELECT a.* FROM ScientificResearch_Test.dbo.继教文件夹 a JOIN cte ON a.父编号 = cte.编号
+)
+INSERT 继教文件夹(编号 ,
+       父编号 ,
+       类型 ,
+       名称 ,
+       完整路径名 ,
+       排序值 ,
+       层级 ,
+       归属人类型 ,
+       归属人编号 ,
+       建立时间 ,
+       备注)
+SELECT * FROM cte;
+
+SET IDENTITY_INSERT 继教文件夹 OFF;
+
+--要导入的试题
+SET IDENTITY_INSERT 继教试题 ON;
+
+WITH cte AS (
+SELECT * FROM ScientificResearch_Test.dbo.继教文件夹  WHERE 名称='共享试题'
+UNION ALL
+SELECT a.* FROM ScientificResearch_Test.dbo.继教文件夹 a JOIN cte ON a.父编号 = cte.编号
+)
+INSERT 继教试题 (编号 ,
+       文件夹编号 ,
+       类型编号 ,
+       题干 ,
+       题目解析 ,
+       难易度 ,
+       建立时间 ,
+       备注)
+SELECT b.* FROM ScientificResearch_Test.dbo.继教试题 b JOIN cte ON b.文件夹编号 = cte.编号;
+
+SET IDENTITY_INSERT 继教试题 off;
+
+--要导入的试题的备选答案
+WITH cte AS (
+SELECT * FROM ScientificResearch_Test.dbo.继教文件夹  WHERE 名称='共享试题'
+UNION ALL
+SELECT a.* FROM ScientificResearch_Test.dbo.继教文件夹 a JOIN cte ON a.父编号 = cte.编号
+),cte试题 AS(
+SELECT b.* FROM ScientificResearch_Test.dbo.继教试题 b JOIN cte ON b.文件夹编号 = cte.编号)
+INSERT 继教试题备选答案
+SELECT c.* FROM ScientificResearch_Test.dbo.继教试题备选答案 c JOIN cte试题 ON c.试题编号=cte试题.编号;
+
+--要导入的试题的正确答案
+WITH cte AS (
+SELECT * FROM ScientificResearch_Test.dbo.继教文件夹  WHERE 名称='共享试题'
+UNION ALL
+SELECT a.* FROM ScientificResearch_Test.dbo.继教文件夹 a JOIN cte ON a.父编号 = cte.编号
+),cte试题 AS(
+SELECT b.* FROM ScientificResearch_Test.dbo.继教试题 b JOIN cte ON b.文件夹编号 = cte.编号)
+INSERT 继教试题正确答案
+SELECT c.* FROM ScientificResearch_Test.dbo.继教试题正确答案 c JOIN cte试题 ON c.试题编号=cte试题.编号;
